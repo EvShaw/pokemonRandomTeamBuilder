706c4be2f853a395b3ed73569ace36d102b30f6f
Evan Shaw <e85.shaw@gmail.com>
2022-04-29 15:09:31 -0700

    cleaned up js files and added pokemon basic api funciton

--- js/main.js
+++ js/main.js
@@ -46,21 +46,72 @@ let teamIndex = 1
 const teamRoster = ['', '', '', '', '', '']
 
 
-//---------------------------------------------------------------------------Top Icons:
+//--------------------------------------------------------------------Pokemon API Fetch:
 
-document.querySelector('.pokeballDisplay').addEventListener('click', iconChange)
+let randNum = Math.floor(Math.random() * 898 ) + 1
 
-function iconChange() {
-  console.log('clicked')
-}
+let url = `https://pokeapi.co/api/v2/pokemon/${randNum}/`
 
+fetch(url) 
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.name)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
+//   //Example fetch using pokemonapi.co
+
+// document.querySelector('button').addEventListener('click', getFetch)
+
+// function getFetch(){
+//   const poke1 = document.querySelector('#poke1').value
+//   const poke2 = document.querySelector('#poke2').value
+//   const url = 'https://pokeapi.co/api/v2/pokemon/'+poke1
+//   const url2 = 'https://pokeapi.co/api/v2/pokemon/'+poke2
+//   let pokeStore = []
+//   let pokeImg = []
+
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {
+
+//         pokeStore.push(data.types[0].type.name)
+//         pokeImg.push(data.sprites.front_shiny)
+        
+//         fetch(url2)
+//         .then(res => res.json()) // parse response as JSON
+//         .then(data => {
+
+//           pokeStore.push(data.types[0].type.name)
+//           pokeImg.push(data.sprites.front_shiny)
+      
+//           if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
+//             document.querySelector('#pokeImg1').src = pokeImg[0]
+//             document.querySelector('#pokeImg2').src = pokeImg[1]
+//             document.querySelector('h2').innerText = " 2x > "
+//           }
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         });
+
+
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+
+
+
+      
+// }
 
 
 
 //------------------------------------------------------------------------ARROWS:
-
-
-
 
 
 
@@ -90,8 +141,6 @@ function shiftLeft() {
 }
 
 
-
-
 //----right arrow: 
 
 document.querySelector('.rightArrow').addEventListener('click', shiftRight)
@@ -117,8 +166,7 @@ function shiftRight() {
 
 }
 
-
-
+//---------------------------------------------------------------------------Top Icons:
 
 function switchIcon(teamIndex) {
   switch (teamIndex) {
